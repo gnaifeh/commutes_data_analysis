3518ab39b19d4702d0b93402b93f24b950b66722
gnaifeh <gpnaifeh@gmail.com>
2015-05-03 08:45:21 +0000

    removed query data file

--- query_data_file.py
+++
@@ -1,5 +0,0 @@
-#data for querying google maps
-gmaps_api_key = "AIzaSyCE9krxNevRUd6UXBppj9l60eQLW1tYInU"
-#data for querying bing maps
-#https://www.bingmapsportal.com/Application
-mmaps_api_key = "An0tPT-fNOUwKKyBWqqw6WdXZCFU929vEssbqMB_i6ADsh5aRUe80zgc_puMiFNQ"
